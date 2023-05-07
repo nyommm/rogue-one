adac427edc1805a0a59122d72fce39504a33c224
Jashandeep Singh <jashan.singh@outlook.in>
2023-05-07 14:26:22 +0530

    handle player movement and quit event

---
+++ lib/__init__.py
@@ -0,0 +1,2 @@
+from .actions import EscapeAction, MovementAction
+from .event_handler import EventHandler
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tcod
+from lib import EventHandler, EscapeAction, MovementAction
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
@@ -9,6 +10,7 @@ def main() -> None:
     tileset = tcod.tileset.load_tilesheet(
         './assets/dejavu10x10_gs_tc.png', 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+    event_handler = EventHandler()
     with tcod.context.new_terminal(
         SCREEN_WIDTH,
         SCREEN_HEIGHT,
@@ -20,8 +22,16 @@ def main() -> None:
         while True:
             root_console.print(x=player_x, y=player_y, string='@')
             context.present(root_console)
+            root_console.clear()
             for event in tcod.event.wait():
-                if event.type == 'QUIT': raise SystemExit()
+                action = event_handler.dispatch(event)
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+                    continue
+                if isinstance(action, EscapeAction):
+                    raise SystemExit()
 
 
 if __name__ == '__main__':
